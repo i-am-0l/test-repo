9d2c9e167c33614afd3a2779ec52800aa0e0ba01
i-am-0l <work.amolpatil@gmail.com>
2026-05-21 11:53:19 +0530

    my 3rd commit

--- src/com/app/tester/TestFunctionality.java
+++ src/com/app/tester/TestFunctionality.java
@@ -6,23 +6,9 @@ class TestFunctionality
 {
 	public static void main(String [] args)
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the dimensions of 1st Box: w*h*d");
-		Box box1 = new Box(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
+		//Box box1 = new Box();
 
-		System.out.println("Enter the dimensions of 2nd Box: w*h*d");
-		Box box2 = new Box(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
-
-		System.out.println(box1.checkEquality(box2)?"SAME":"Different");
-
-		System.out.println("Enter the dimensions of the Box: w*h*d");
-		Box box = new Box(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
-
-		System.out.println("Enter the offsets of New Box: width Offset, height Offset, depthOffset");
 		
-		System.out.println(box.createNewBox(sc.nextDouble(), sc.nextDouble(), sc.nextDouble()).getBoxDetails());
-
-		sc.close();
 		System.out.println("main over");
 	}	
 
